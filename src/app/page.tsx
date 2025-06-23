87056e2f341a3550e1f6c8c23e1e030df9c67186
Devansh Rai <devanshrai96@gmail.com>
2025-06-23 23:19:30 +0530

    session providor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ import LandingPage from "./landing-page";
 
 export default async function Home() {
   return (
-    <SessionProvider>
+    <>
       <Header />
 
       <main className="flex min-h-screen flex-col items-center relative p-24 bg:dark z-10 antialiased">
         <LandingPage />
       </main>
-    </SessionProvider>
+    </>
   );
 }
